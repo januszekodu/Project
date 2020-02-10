dc6bf6aa3254a0e27dce95c97d19aa4b4da15286
janvsz <49714370+janvsz@users.noreply.github.com>
2020-02-10 22:48:13 +0100

    Update FunctionParser.h
    Update FunctionParser

--- FunctionParser/FunctionParser.h
+++ FunctionParser/FunctionParser.h
@@ -5,6 +5,7 @@
 #ifndef IO_PROJECT_2_FUNCTIONPARSER_H
 #define IO_PROJECT_2_FUNCTIONPARSER_H
 #include<string>
+#include<vector>
 
 
 namespace functionParser
@@ -13,20 +14,22 @@ namespace functionParser
 
     std::string get_include(const std::string &);
 
-    bool is_namespace(const std::string &);
+    bool is_namespace(const std::string);
 
-    std::string get_namespace(const std::string &);
-
-    bool is_function_header(const std::string &);
-
-    std::string get_function_header(const std::string &);
-
-    bool is_function_call(const std::string &);
-
-    std::string get_function_call(const std::string &);
+    std::string get_namespace(const std::string);
 
     bool is_block_opening_bracket(const std::string &);
 
     bool is_block_closing_bracket(const std::string &);
+
+    bool is_block_closing_bracket(const std::string &);
+
+    bool is_function_header(std::string);
+    std::string get_function_header(std::string);
+
+    bool is_function_call(std::string);
+    std::vector<std::string> get_function_calls(std::string);
+    bool has_namespace(std::string);
+    std::string get_file_path_base_from_cpp(std::string);
 }
 #endif //IO_PROJECT_2_FUNCTIONPARSER_H
